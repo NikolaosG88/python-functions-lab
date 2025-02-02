d8d39d83c1f81451f6f9791ab9368bc86093781a
NikolaosG <Nikolaos_Gkazgkas@outlook.com>
2025-02-02 19:15:22 +0000

    basic Python exploration

--- exercises.py
+++ exercises.py
@@ -37,4 +37,23 @@ def simple_interest(principal, rate, time):
 print('Exercise 2:', simple_interest(1000, 5, 2))
 print('Exercise 2a:', simple_interest(1500, 3.5, 5))
 
+#---------------3---------------#
+# Exercise 3: Apply a Discount
+#
+# Write a function named `apply_discount` that takes a product's price and a discount percentage (from 0 to 100).
+# Return the new price after applying the discount.
+#
+# Examples:
+# apply_discount(100, 25) should return 75.
+# apply_discount(80, 10) should return 72.
+#
+# Define your function and call it to display the discounted price.
+def apply_discount(price, discount_persenatge ):
+   new_price = price - (price * (discount_persenatge * 0.01))
+   return new_price
+
+print('Exercise 3:', apply_discount(100, 25))
+print('Exercise 3a:', apply_discount(80, 10))
+
+
 
